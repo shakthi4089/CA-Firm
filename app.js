bd8590bd1841646697399333bddfeb188c9d56a6
Shakthi Eswaran <shakthi4089@gmail.com>
2026-07-30 23:16:24 +0530

    Configure EJS and create home page

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+// Set EJS as template engine
+app.set("view engine", "ejs");
+
+// Views folder
+app.set("views", path.join(__dirname, "views"));
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Welcome to CA Firm Management System");
+    res.render("pages/home");
 });
 
-const PORT = 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
